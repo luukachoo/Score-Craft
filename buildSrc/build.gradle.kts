1a9f59591236dc2f982f3870119d98f9bfe2e8ab
Aleksandre Toria <aleksandretoria1@gmail.com>
2024-02-26 21:34:13 +0400

    Fixed hilt exception

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
     implementation("com.android.tools.build:gradle:8.2.2")
+    implementation("com.squareup:javapoet:1.13.0")
 }
 
 val compileKotlin: KotlinCompile by tasks
